76d3dc80ad5b9ea6ba409a695b8ee9ecee519694
Jacek Lyp <jaceklyp@gmail.com>
2013-04-17 12:52:05 +0200

    First class

---
+++ src/Profiler.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jacek
+ * Date: 17.04.2013
+ * Time: 12:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Profiler {
+
+}
